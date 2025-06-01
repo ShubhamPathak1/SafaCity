62928efbf0036f81243f813432286734574a8707
shubhampathak1 <pathakshubhu123@gmail.com>
2025-06-01 19:05:46 +0545

    initial app working, tells waste degradable

---
+++ services/geminiApi.ts
@@ -0,0 +1,32 @@
+export const sendToGemini = async (base64Image: string) => {
+  const API_KEY = "AIzaSyAeOXu1alpnypG1bEPm9Ez8-kteRkRf_gM";
+  const endpoint = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
+  
+
+  const payload = {
+    contents: [
+      {
+        parts: [
+          {
+            text: "Tell the name of this waste. And tell whether it is degradable or non-degradable, in this format: Banana Peel : Biodegradable. Please dont add any other texts.",
+          },
+          {
+            inlineData: {
+              mimeType: "image/jpeg",
+              data: base64Image,
+            },
+          },
+        ],
+      },
+    ],
+  };
+
+  const res = await fetch(endpoint, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload),
+  });
+
+  const json = await res.json();
+  return json?.candidates?.[0]?.content?.parts?.[0]?.text ?? "No response from Gemini.";
+};
